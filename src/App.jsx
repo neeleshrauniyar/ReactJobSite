5111a39fcc0a7bbb4b4246c5fb24ee0c79c38706
neeleshrauniyar <rauniyar.neelesh@gmail.com>
2024-07-06 19:38:25 +0545

    add description shortner with state management usestate

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HeroCards from './components/HeroCards'
 import JobListings from './components/JobListings'
-
+import ViewAllJobs from './components/ViewAllJobs'
 const App = () => {
   return (
     <>
@@ -11,17 +11,8 @@ const App = () => {
     <Hero />
     <HeroCards />
     <JobListings />
-    
+    <ViewAllJobs />
 
-    
-
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
     </>
   )
 }
